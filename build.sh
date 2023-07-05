20adefcfb939b7672fe2f6f76b6e2b99ff585edb
Code-4ge <panmandneeraj@gmail.com>
2023-07-05 16:22:50 +0530

    modifiy vercel setting

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 # Create a virtual environment
-echo "Creating a virtual environment..."
-python3.9 -m venv venv
-venv\Scripts\activate
+# echo "Creating a virtual environment..."
+# python3.9 -m venv venv
+# venv\Scripts\activate
 
-echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
+# echo "Installing the latest version of pip..."
+# python -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
-
+python3.9 -m pip install -r requirements.txt
+echo "Build end"
 # Collect static files
 echo "Running..."
-python index.py
+python3.9 index.py
